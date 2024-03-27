49503a15e33990f408df0853e7f2387e6c9ac0d8
AwaisTahir786 <tahirawais111@gmail.com>
2024-03-27 19:31:05 +0500

    pushe

---
+++ productizefast-boiler-plate-main/components/FAQ/faqData.tsx
@@ -0,0 +1,31 @@
+import { FAQ } from "@/types/faq";
+
+const faqData: FAQ[] = [
+  {
+    id: 1,
+    quest: " I'm not very technical. Can I still use the Productize Fast Boilerplate?",
+    ans: "Absolutely! Our boilerplate is designed to minimize technical hurdles. We provide clear setup instructions, and you'll be able to customize your landing page and workflows without extensive coding knowledge.",
+  },
+  {
+    id: 2,
+    quest: " Can I customize the look and feel of the boilerplate?",
+    ans: "Yes! While the landing page provides a strong starting point, you can easily adjust colors, fonts, and content to match your brand. Additionally, the workflows in Trello are fully adaptable to your service process.",
+  },
+  {
+    id: 3,
+    quest: " What if I get stuck or have questions during setup?",
+    ans: " We offer [insert your level of support - email, basic documentation, community forum, etc. We're committed to helping you launch your productized service successfully",
+  },
+  {
+    id: 4,
+    quest: " Do I need separate accounts for Trello, PayPal, and Razorpay?",
+    ans: " Yes, you'll need to set up your own accounts with these services. The Productize Fast Boilerplate will streamline the integration for a seamless user experience.",
+  },
+  {
+    id: 5,
+    quest: "Is this a one-time purchase or a subscription?",
+    ans: "The Productize Fast Boilerplate is a one-time purchase, giving you full access to the code and resources for unlimited use.",
+  },
+];
+
+export default faqData;
---
+++ productizefast-boiler-plate-main/components/Features/featuresData.tsx
@@ -0,0 +1,48 @@
+import { Feature } from "@/types/feature";
+
+const featuresData: Feature[] = [
+  {
+    id: 1,
+    icon: "/images/icon/icon-01.svg",
+    title: "Effortless Onboarding",
+    description:
+      " Automate client welcomes and task creation with integrated Trello boards."
+  },
+  {
+    id: 2,
+    icon: "/images/icon/icon-02.svg",
+    title: "Secure Payments",
+    description:
+      "Collect recurring payments easily with PayPal and Razorpay.",
+  },
+  {
+    id: 3,
+    icon: "/images/icon/icon-03.svg",
+    title: "Customizable Workflow",
+    description:
+      "Tailor your service processes to match your unique expertise.",
+  },
+  {
+    id: 4,
+    icon: "/images/icon/icon-04.svg",
+    title: "Accelerated Launch",
+    description:
+      "Skip the technical headaches and start earning faster.",
+  },
+  // {
+  //   id: 5,
+  //   icon: "/images/icon/icon-05.svg",
+  //   title: "DB, Auth and Stripe",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+  // {
+  //   id: 6,
+  //   icon: "/images/icon/icon-06.svg",
+  //   title: "Regular Free Updates",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+  // },
+];
+
+export default featuresData;
